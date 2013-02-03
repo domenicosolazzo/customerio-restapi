476ab31d018cef748e1c61ad4e354a5d8b049059
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-02-03 22:23:55 +0100

    Refactoring in the flask app. Added documentation to the methods in the flask app. Fixed retrieve Email to return only one result

--- app.py
+++ app.py
@@ -26,6 +26,11 @@ app = json_flask_app(__name__)
 cio = CustomerIO(siteKey, apiKey)
 
 def parseCustomerObject(customer):
+    """
+    Parse the returned data given a customer in input
+    :param customer:
+    :return:
+    """
     return {
         'customer':{
             'id':customer[0],
@@ -35,6 +40,13 @@ def parseCustomerObject(customer):
     }
 
 def __signupCustomer(email, contactCustomerIO=False):
+    """
+    Lookup a customer by email and it adds to the database if it is not present.
+    It contacts Customer.IO if the customer is inserted in the database or contactCustomerIO is true
+    :param email: The customer's email
+    :param contactCustomerIO: True if the service is forced to contact Customer.IO, false otherwise
+    :return: The returned data
+    """
     customer = facade.retrieveEmail(email)
     signup = contactCustomerIO
     if not customer:
@@ -57,12 +69,22 @@ def hello():
 
 @app.route('/signup/<email>', methods=['GET'])
 def signupCustomer(email):
+    """
+    It signup a customer and contact Customer.IO if the customer is not present in the db
+    :param email: The Customer's email
+    :return:The returned json data
+    """
     data = __signupCustomer(email)
     # Return the data
     return jsonify(data=data)
 
 @app.route('/signup/<email>/force', methods=['GET'])
 def forceSignup(email):
+    """
+    It signup a customer and contact Customer.IO if the customer is not present in the db
+    :param email: The Customer's email
+    :return:The returned json data
+    """
     data = __signupCustomer(email, True)
     # Return the data
     return jsonify(data=data)
@@ -70,19 +92,22 @@ def forceSignup(email):
 
 @app.route('/customers/<email>/events/<eventname>', methods=['GET'])
 def sendEvent(email, eventname):
+    """
+    It send an event for a given customer's email. It contact Customer.IO given the event's name
+    :param email: The customer's email
+    :param eventname: The event name
+    :return: The returned json data
+    """
     customer = facade.retrieveEmail(email)
-    if not customer or len(customer) <= 0:
+    if not customer:
         raise Exception("Customer is not present.")
-    customer = customer[0]
+
     cio.track(customer_id=customer[0], name=eventname)
-    return jsonify(data={
-        'customer':{
-            'id':customer[0],
-            'email':customer[1]
-        },
-        'event':eventname,
-        'date': datetime.datetime.utcnow().isoformat()
-    })
+
+    data = parseCustomerObject(customer)
+    data['event'] = eventname
+
+    return jsonify(data=data)
 
 if  __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
